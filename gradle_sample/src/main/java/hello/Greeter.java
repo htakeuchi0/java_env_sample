290dfb0ba4ed5753f607d5bc809d1f208d071c6b
htakeuchi0 <cis.n6.v56ofv.i46.v7.i@gmail.com>
2019-10-05 10:02:35 +0900

    gradleサンプルを追加

---
+++ gradle_sample/src/main/java/hello/Greeter.java
@@ -0,0 +1,11 @@
+package hello;
+
+/**
+ * 以下で提示されているサンプルクラス．
+ * https://qiita.com/marutanseijin/items/b2897539ea94d67739e6
+ */
+public class Greeter {
+  public String sayHello() {
+    return "Hello world!";
+  }
+}
